05f3b0ccd2f4f7b2ee67bd0f25bf93ef10e6ec47
Ricco <ricco.amezcua@gmail.com>
2021-10-30 15:04:16 -0500

    Custom Form object works better

--- src/featues/forms/formikComponentsPlay.tsx
+++ src/featues/forms/formikComponentsPlay.tsx
@@ -1,7 +1,14 @@
 // Todo
 // add checkbox and select
 // checkout formik helper for disabling button
-import { FieldHookConfig, Form, Formik, FormikHelpers, useField } from "formik"
+import {
+  FieldHookConfig,
+  Form,
+  Formik,
+  FormikHelpers,
+  useField,
+  useFormikContext,
+} from "formik"
 
 import { ReactNode } from "react"
 
@@ -26,6 +33,15 @@ const MyTextInput = (props: FieldHookConfig<string>) => {
   )
 }
 
+const MyButton = () => {
+  const { isSubmitting } = useFormikContext()
+  return (
+    <button type="submit" disabled={isSubmitting}>
+      Submit
+    </button>
+  )
+}
+
 interface MyValues {
   [field: string]: any
 }
@@ -34,15 +50,35 @@ interface MyFormProps {
   initialValues: MyValues
   onSubmit: (
     values: MyValues,
-    formikHelpers: FormikHelpers<MyValues>
-  ) => void | Promise<any>
+    setSubmitting: (isSubmitting: boolean) => void
+  ) => void
   children: ReactNode
 }
 
 function MyForm({ initialValues, onSubmit, children }: MyFormProps) {
+  const validate = (values: MyValues) => {
+    const errors: any = {}
+    if (!values.firstName) {
+      errors.firstName = "Required"
+    } else if (values.firstName.length > 15) {
+      errors.firstName = "Must be 15 characters or less"
+    }
+    return errors
+  }
+
+  const submitWrapper = (
+    values: MyValues,
+    { setSubmitting }: FormikHelpers<MyValues>
+  ) => {
+    onSubmit(values, setSubmitting)
+  }
   return (
     <>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={submitWrapper}
+        validate={validate}
+      >
         <Form>{children}</Form>
       </Formik>
     </>
@@ -55,30 +91,46 @@ function FormikComponentsPlay() {
     lastName: "",
     email: "",
     acceptedTerms: false, // added for our checkbox
+    dogs: ["poo", ""],
     jobType: "", // added for our select
   }
+  // on submit scenario
+  // submit to server
+  // if error, show error
+  // set submitting to false
+  // else data is good
+  // show data? go to next page?
 
-  const onSubmit = (
+  // anytime I want to submit something, my submit function must take a setSubmitting, and values parameter
+  function sendData(
     values: MyValues,
-    { setSubmitting }: FormikHelpers<MyValues>
-  ) => {
+    setSubmitting: (isSubmitting: boolean) => void
+  ) {
     setTimeout(() => {
-      alert(JSON.stringify(values, null, 2))
+      console.log("values", values)
       setSubmitting(false)
-    }, 400)
+    }, 500)
   }
+
+  // const onSubmit = (
+  //   values: MyValues,
+  //   { setSubmitting }: FormikHelpers<MyValues>
+  // ) => {
+  //   setTimeout(() => {
+  //     console.log("being caled")
+  //     alert(JSON.stringify(values, null, 2))
+  //     setSubmitting(false)
+  //   }, 400)
+  // }
 
   return (
     <>
       <h1>Subscribe!</h1>
-      <MyForm initialValues={initValues} onSubmit={onSubmit}>
-        {() => (
-          <>
-            <MyTextInput name="firstName" type="text" placeholder="Jane" />
-
-            <button type="submit">Submit</button>
-          </>
-        )}
+      <MyForm initialValues={initValues} onSubmit={sendData}>
+        <MyTextInput name="firstName" type="text" placeholder="Jane" />
+        <MyTextInput name="dogs[0]" type="text" placeholder="" />
+        <MyTextInput name="dogs[1]" type="text" placeholder="" />
+        <MyButton />
       </MyForm>
     </>
   )
