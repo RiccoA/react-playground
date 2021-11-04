12c8433c8b467599679fe7c7542dc35ebf13f043
Ricco <ricco.amezcua@gmail.com>
2021-11-04 00:26:07 -0500

    Successfully mocked a hook

--- src/featues/testingHooks/PokemonNamePlate.test.tsx
+++ src/featues/testingHooks/PokemonNamePlate.test.tsx
@@ -1,32 +1,19 @@
 import { render } from "@testing-library/react"
 import { PokemonNamePlate } from "./PokemonNamePlate"
-// import { usePokemonNamePlate } from "./pokeNamePlate.hook"
+import { usePokemonNamePlate } from "./pokeNamePlate.hook"
 
-// const mockMyHook = jest.fn()
+jest.mock("./pokeNamePlate.hook")
 
-jest.mock("./pokeNamePlate.hook", () => ({
-  usePokemonNamePlate: () => ({
-    isLoading: false,
-    name: "pikachu",
-    favoriteBerries: "Loom",
-  }),
-}))
-
-// const mockUseFetchPokemon = jest.fn()
-// jest.mock("./pokeNamePlate.hook", () => {
-//   return jest.fn(() => ({
-//     usePokemonNamePlate: () => {
-//       return { isLoading: false, name: "pikachu", favoriteBerries: "Loom" }
-//     },
-//   }))
-// })
+const usePokemonNamePlateMock = usePokemonNamePlate as jest.MockedFunction<
+  typeof usePokemonNamePlate
+>
 
 it("Can show name", () => {
-  // usePokemonNamePlate.mockReturnValues(() => ({
-  //   isLoading: true,
-  //   name: "Pikachu",
-  //   favoriteBerries: "Loom",
-  // }))
+  usePokemonNamePlateMock.mockReturnValue({
+    isLoading: true,
+    name: "pikachu",
+    favoriteBerries: "Loom",
+  })
   const { container } = render(<PokemonNamePlate />)
   expect(container).toHaveTextContent("pikachu")
   expect(container).toHaveTextContent("Loom")
